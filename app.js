a14a9ea7a6047545f81f79d07870aa26a39b33a0
kamranalam1 <kamranalam098@gmail.com>
2021-08-23 14:21:12 +0500

    ssome comde

---
+++ app.js
@@ -0,0 +1,11 @@
+// var nameA = "kamran "
+// var nameB = "Ahmer"
+// var nameC = "Saqib"
+// ;
+// document.write ( "This is my name " + nameA + "<br>"+ "this is  my name" + nameB + "<br>" + nameC);
+// // console.log(nameA);
+
+var num1 = parseInt(prompt("enter num ", "your here "))
+var num2 = parseInt(prompt("enter num ", "your here ") ) 
+var result = num1 + num2;
+document.write(result)
